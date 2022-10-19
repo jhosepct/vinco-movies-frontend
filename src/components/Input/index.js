ec26eee9cc137bf0ef275d1b2b32890ec631aeb7
k3v1n <kevinjhosepct@gmail.com>
2022-10-19 16:56:26 -0500

    Create Input.stories and refactor styles

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,11 +1,29 @@
 import { InputContainer, StyledInput } from "./styles";
 
+import PropTypes from "prop-types";
+import { colors } from "../../assets/colors";
+
 export const Input = ({ leftIcon, rightIcon, ...props }) => {
   return (
-    <InputContainer>
+    <InputContainer {...props}>
       {leftIcon}
       <StyledInput placeholder="placeholder" {...props} autoComplete="off" />
       {rightIcon}
     </InputContainer>
   );
+};
+
+Input.proTypes = {
+  leftIcon: PropTypes.element,
+  rightIcon: PropTypes.element,
+  color: PropTypes.string,
+  colorHover: PropTypes.string,
+  background: PropTypes.string,
+};
+
+Input.defaultProps = {
+  placeholder: "Placeholder",
+  background: "transparent",
+  color: colors.defaultPink,
+  colorHover: colors.darkPink,
 };
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -4,18 +4,19 @@ import { typography } from "../../assets/typography";
 
 export const InputContainer = styled.div`
   ${typography.body2}
+  background: ${(props) => props.background};
   display: flex;
   padding: 0.5rem;
   align-items: center;
   gap: 0.5rem;
-  border: 1px solid ${colors.defaultPink};
+  border: 1px solid ${(props) => props.color};
   border-radius: 0.5rem;
   color: ${colors.darkGray};
   &:hover {
-    border: 1px solid ${colors.darkPink};
+    border: 1px solid ${(props) => props.colorHover};
   }
   &:active {
-    border: 1px solid ${colors.darkPink};
+    border: 1px solid ${(props) => props.colorHover};
   }
 `;
 
@@ -24,6 +25,6 @@ export const StyledInput = styled.input`
   outline: none;
   font-size: 14px;
   width: 100%;
-  background: ${colors.background};
+  background: ${(props) => props.background};
   color: ${colors.white};
 `;
