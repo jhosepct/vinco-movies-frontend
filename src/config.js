cabc31438427ff073563b5a7514d4d438ae5d759
k3v1n <kevinjhosepct@gmail.com>
2022-10-21 09:22:52 -0500

    Modificate base_uri local for imsonmia

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const BASE_URI = "http://localhost:4000/";
+export const BASE_URI = "https://vinco-movies-api.herokuapp.com";
+export const BASE_URI_LOCAL = "http://127.0.0.1:4000";
 export const BASE_URL_MOVIES = "https://api.themoviedb.org/3";
 export const tokenKey = "movie-token";
 export const id = "movie-user-id";
