70630f5261ad19178cb1cb85152d001a8f1afb2e
k3v1n <kevinjhosepct@gmail.com>
2022-10-21 11:19:02 -0500

    Modificate env

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,6 +1,6 @@
 import { apiFetchMovies } from "./api-fetch";
 
-const API_KEY = process.env["REACT_APP_API_KEY"];
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function searchMovies(query) {
   return apiFetchMovies
